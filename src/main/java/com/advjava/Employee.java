ba90f9e864437a4e877798b5070c80165c8877ae
Dhiraj <dhirajhegade10@gmail.com>
2025-09-18 20:56:19 +0530

    Employee Task Servlet with JDBC

--- src/main/java/com/advjava/Employee.java
+++ src/main/java/com/advjava/Employee.java
@@ -1,6 +1,12 @@
 package com.advjava;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,20 +18,22 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Employee extends HttpServlet
 {
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
 		String name = req.getParameter("name");
 		String last = req.getParameter("last");
-		
-		String dob = req.getParameter("dob");		
+
+		String dob = req.getParameter("dob");
+		LocalDate dob1 = LocalDate.parse(dob);
+
 		String gender = req.getParameter("gender");
-		
+
 		String mob = req.getParameter("mob");
 		long mob1 = Long.parseLong(mob);
-		
+
 		String email = req.getParameter("email");
 		String pswd = req.getParameter("pswd");
-		
+
 		String exp = req.getParameter("exp");
 		float exp1 = Float.parseFloat(exp);
 
@@ -34,45 +42,61 @@ public class Employee extends HttpServlet
 		String addr = req.getParameter("addr");
 		String dept = req.getParameter("dept");
 		String desig = req.getParameter("desig");
-		
+
 		String doj = req.getParameter("doj");
-		
+		LocalDate doj1 = LocalDate.parse(doj);
+
 		String sal = req.getParameter("sal");
 		float sal1 = Float.parseFloat(sal);
-		
-		
-		System.out.println("----- Data coming from Frontend to Backend throught the server-----");
-		System.out.println();
-		
-		System.out.println("First name: " + name);
-		System.out.println("First last: " + last);
-		System.out.println("Date of Birth: " + dob);
-		System.out.println("Gender: " + gender);
-		System.out.println("Mobile no: " + mob1);
-		System.out.println("Email: " + email);
-		System.out.println("Password: " + pswd);
-		System.out.println("Experience: " + exp1);
-		System.out.println("Country: " + country);
-		System.out.println("State: " + state);
-		System.out.println("Address: " + addr);
-		System.out.println("Department: " + dept);
-		System.out.println("Designation: " + desig);
-		System.out.println("Date of joining: " + doj);
-		System.out.println("Expected salary in lpa: " + sal1);
 
-
+		// To generate dynamic response
+		PrintWriter out = res.getWriter();
 		
+		// DB connect
 
-		
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/advjava", "root", "12345");
+			PreparedStatement ps = c.prepareStatement(
+					"INSERT INTO emp (first_name, last_name, dob, gender, mob, email, pswd, exp, country, state, addr, dept, desig, doj, salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			
+			//set the values
+			ps.setString(1, name);
+			ps.setString(2, last);
+			ps.setDate(3, java.sql.Date.valueOf(dob1));
+			ps.setString(4, gender);
+			ps.setLong(5, mob1);
+			ps.setString(6, email);
+			ps.setString(7, pswd);
+			ps.setFloat(8, exp1);
+			ps.setString(9, country);
+			ps.setString(10, state);
+			ps.setString(11, addr);
+			ps.setString(12, dept);
+			ps.setString(13, desig);
+			ps.setDate(14, java.sql.Date.valueOf(doj1));
+			ps.setFloat(15, sal1);
 
-		
+			int chk = ps.executeUpdate();
+			if (chk > 0)
+			{
+				
+				out.println("alert('Data inserted successfully!');");
+				//out.println("<h1 style='color:green'>" + "Data is Inserted Successfully" + "<h1/>");
+			} 
+			
+			else
+			{
+				out.println("<h1 style = 'color:red'>" + "Data is not Inserted " + "<h1/>");
 
-		
-		
+			}
+		}
 
+		catch (Exception e)
+		{
+			out.println("Error = " + e.getMessage());
+		}
 
-		
-
-		
 	}
 }
